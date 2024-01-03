a8f39c124345868ff9611cde84fa086457b3a8fc
David0xfff <jdavidguzman0101@gmail.com>
2024-01-02 20:24:44 -0500

    first commit

---
+++ variables1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Ejercicio donde se declara dos variables una option y otra llamada result, se inicializan y seimprimen sus valores 
+
+option=2
+result=32
+echo "la opcion $option y el resultado es $result"
+
